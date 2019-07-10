66a1d9002f3875f99846514c8d6f9a7c7af1a275
VictorJiangXin <921320448@qq.com>
2019-07-10 11:31:27 +0800

    add demo for test

--- src/demo.py
+++ src/demo.py
@@ -18,3 +18,18 @@ class Segmentation(object):
                 str_seg += ' '
         result = str_seg.split()
         return result
+
+
+seg = Segmentation('model/linear_crf.model')
+sen1 = '今晚的月色真美呀！'
+sen2 = '生命在于奋斗！'
+sen3 = '小哥哥，别复习了，来玩吧！'
+
+print('测试句:', sen1)
+print('分词后:', seg.seg(sen1))
+
+print('测试句:', sen2)
+print('分词后:', seg.seg(sen2))
+
+print('测试句:', sen3)
+print('分词后:', seg.seg(sen3))
